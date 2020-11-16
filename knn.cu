3a5873576d50945cb932df00fe2b53b98bf3c200
Ravindu Sandeepa <ravindursr@gmail.com>
2020-11-16 20:05:57 +0530

    read ref and query points

--- knn.cu
+++ knn.cu
@@ -4,16 +4,33 @@
 
 //#include <stdlib.h>
 #include <stdio.h>
+#include "utilities.h"
 
-int main(){
+__global__ void calc_dist_global_mem(float *refP, float *queryP, int n_refP, int n_queryP, int n_dim){
+
+    
+
+}
+
+int main()
+{
+
+    int n_refPoints = 8192;
+    int n_queryPoints = 2048;
+    int n_dimentions = 4;
+
+    float *refPoints_h;
+    ClassAndDist *classAndDistArr_h;
+    float *queryPoints_h;
 
     cudaError_t error;
     cudaDeviceProp prop;
     int device_count;
+    int warpSize = 32;
 
-    error = cudaGetDeviceCount (&device_count);
+    error = cudaGetDeviceCount(&device_count);
 
-    if(error != cudaSuccess)
+    if (error != cudaSuccess)
     {
         printf("Error: %s\n", cudaGetErrorString(error));
         exit(-1);
@@ -21,11 +38,36 @@ int main(){
 
     error = cudaGetDeviceProperties(&prop, 0);
 
+    warpSize = prop.warpSize;
+
     printf("device count : %d\n", device_count);
     printf("device name : %s\n", prop.name);
-    printf("device total global memory(KB) : %d\n", prop.totalGlobalMem/1024);
+    printf("device total global memory(KB) : %d\n", prop.totalGlobalMem / 1024);
     printf("max texture dimension x : %d    y : %d\n", prop.maxTexture2D[0], prop.maxTexture2D[1]);
+
+    refPoints_h = (float *)malloc(sizeof(float) * n_dimentions * n_refPoints);
+    classAndDistArr_h = (ClassAndDist *)malloc(sizeof(ClassAndDist) * n_refPoints * n_queryPoints);
+    queryPoints_h = (float *)malloc(sizeof(float) * n_dimentions * n_queryPoints);
+
+    char *refPointsFileName = "testData8192_4.csv";
+    char *queryPointsFileName = "queryPoints_4.csv";
+
+    readRefPoints(refPointsFileName, refPoints_h, classAndDistArr_h, n_refPoints, n_queryPoints, n_dimentions);
+
+    // for (int i = 0; i < noOfRefPoints; i++)
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     printf("%d  %f  %f  %f  %f  %d\n", i, refPoints_h[i*n_dimentions + 0], refPoints_h[i*n_dimentions + 1], refPoints_h[i*n_dimentions + 2], refPoints_h[i*n_dimentions + 3], classAndDistArr_h[i].cls);
+    // }
+
+    readQueryPoints(queryPointsFileName, queryPoints_h, n_dimentions);
+
+    // for (int i = 0; i < n_queryPoints; i++)
+    // {
+    //     printf("%d  %f  %f  %f  %f \n", i, queryPoints_h[i*n_dimentions + 0], queryPoints_h[i * n_dimentions + 1], queryPoints_h[i*n_dimentions + 2], queryPoints_h[i*n_dimentions + 3]);
+    // }
+
+
 
     return 0;
 }
-
---
+++ utilities.h
@@ -0,0 +1,104 @@
+#include <stdlib.h>
+
+#define MAX_SOURCE_SIZE (0x100000)
+
+
+// struct ClassAndDist
+// {
+//     float distance;
+//     int class;
+// };
+typedef struct
+{
+    int cls;
+    float distance;
+    
+} ClassAndDist;
+
+char *readKernel(char *kernelFileName)
+{
+
+    char *source_str = (char *)malloc(MAX_SOURCE_SIZE);
+    FILE *fp = fopen(kernelFileName, "r");
+
+    if (!fp)
+    {
+        fprintf(stderr, "Failed to load kernel.\n");
+        exit(1);
+    }
+
+    size_t source_size = fread(source_str, 1, MAX_SOURCE_SIZE, fp);
+
+    fclose(fp);
+
+    return source_str;
+}
+
+void readRefPoints(char *fileName, float *refPoints_h, ClassAndDist *classAndDistArr_h, int noOfRefPoints, int noOfQueryPoints, int noOfAttributes){
+
+    FILE *dataFile = fopen(fileName, "r");
+
+    int index = -1;
+
+        while (!feof(dataFile))
+    {
+        char line[1024];
+
+        fgets(line, 1024, dataFile);
+
+        if (index >= 0)
+        {
+
+            //only read 4 attributes + class
+            // float *rowOfRefPoints = (float *)malloc(sizeof(float) * noOfAttributes);
+
+            // sscanf(line, "%f,%f,%f,%d", &rowOfRefPoints[0], &rowOfRefPoints[1], &rowOfRefPoints[2], &classAndDistArr_h[index].class);
+
+            int clsTemp = 0;
+            sscanf(line, "%f,%f,%f,%f,%d", &refPoints_h[index*noOfAttributes + 0], &refPoints_h[index*noOfAttributes + 1],&refPoints_h[index*noOfAttributes + 2], &refPoints_h[index*noOfAttributes + 3], &clsTemp);
+
+            for (int i = 0; i < noOfQueryPoints; i++)
+            {
+                classAndDistArr_h[i* noOfRefPoints + index].cls = clsTemp;
+            }
+            
+            // refPoints_h[index] = rowOfRefPoints;
+        }
+
+        index++;
+    }
+
+    fclose(dataFile);
+
+}
+
+
+void readQueryPoints(char *fileName, float *queryPoints_h, int noOfAttributes){
+
+    FILE *dataFile = fopen(fileName, "r");
+
+    int index = -1;
+
+        while (!feof(dataFile))
+    {
+        char line[1024];
+
+        fgets(line, 1024, dataFile);
+
+        if (index >= 0)
+        {
+
+            //only read 4 attributes
+            // float *rowOfQueryPoints = (float *)malloc(sizeof(float) * noOfAttributes);
+
+            // sscanf(line, "%f,%f,%f,%d", &rowOfRefPoints[0], &rowOfRefPoints[1], &rowOfRefPoints[2], &classAndDistArr_h[index].class);
+            sscanf(line, "%f,%f,%f,%f", &queryPoints_h[index*noOfAttributes + 0], &queryPoints_h[index*noOfAttributes + 1],&queryPoints_h[index*noOfAttributes + 2], &queryPoints_h[index*noOfAttributes + 3]);
+            
+            // queryPoints_h[index] = rowOfQueryPoints;
+        }
+
+        index++;
+    }
+
+    fclose(dataFile);
+}
