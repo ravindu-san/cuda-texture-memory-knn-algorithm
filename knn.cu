3c5ff6bbf8a9c9635d987f2d78ced5b2867a175b
Ravindu Sandeepa <ravindursr@gmail.com>
2020-11-18 21:18:54 +0530

    addded kernel to sorting the distances using bitonic sort

--- knn.cu
+++ knn.cu
@@ -12,27 +12,89 @@ __global__ void calc_dist_global_mem(float *refP, float *queryP, float *distance
     unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x; //ref points vary across x axis of grid
     unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y; //query points vary across y axis of grid
 
-    float sqrd_dist;
+    if(xIndex < n_refP && yIndex < n_queryP){
 
-    for (int i = 0; i < n_dim; i++)
-    {
+        float sqrd_dist;
 
-        float diff = refP[xIndex * n_dim + i] - queryP[yIndex * n_dim + i];//ref points & query points are in row major order
-        sqrd_dist += diff * diff;
+        for (int i = 0; i < n_dim; i++)
+        {
+
+            float diff = refP[xIndex * n_dim + i] - queryP[yIndex * n_dim + i];//ref points & query points are in row major order
+            sqrd_dist += diff * diff;
+        }
+
+        distances[yIndex * n_refP + xIndex] = sqrd_dist;
+
+        if(xIndex == 0 && yIndex == 1023){
+            printf("distance of ref %d q %d : %f\n", xIndex, yIndex,sqrd_dist);
+        }
+
     }
-
-    distances[yIndex * n_refP + xIndex] = sqrd_dist;
-
-    // if(xIndex == 0 && yIndex == 0){
-        printf("distance of ref %d q %d : %f\n", xIndex, yIndex,sqrd_dist);
-    // }
+    
 }
+
+__global__ void sort_dist_bitonic(float *distances, int *clases, int n_refP, int n_queryP,const uint stage, const uint passOfStage){
+
+    // uint threadId = get_global_id(0);
+    unsigned int xIndex = blockIdx.x * blockDim.x + threadIdx.x;
+    unsigned int yIndex = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if(xIndex < n_refP/2 && yIndex < n_queryP){
+
+        unsigned int pairDistance = 1 << (stage - passOfStage);
+        unsigned int blockWidth = 2 * pairDistance;
+        unsigned int temp;
+        bool compareResult;
+    
+        unsigned int leftId = (xIndex & (pairDistance - 1)) + (xIndex >> (stage - passOfStage)) * blockWidth;
+        unsigned int rightId = leftId + pairDistance;
+    
+        float leftElement, rightElement;
+        float greater, lesser;
+        int leftElement_cls, rightElement_cls, greater_cls, lesser_cls;
+
+        leftElement = distances[yIndex * n_refP + leftId];
+        rightElement =distances[yIndex * n_refP +rightId];
+    
+        leftElement_cls = clases[yIndex * n_refP + leftId];
+        rightElement_cls = clases[yIndex * n_refP +rightId];
+    
+        unsigned int sameDirectionBlockWidth = xIndex >> stage;
+        unsigned int sameDirection = sameDirectionBlockWidth & 0x1;
+    
+        temp = sameDirection ? rightId : temp;
+        rightId = sameDirection ? leftId : rightId;
+        leftId = sameDirection ? temp : leftId;
+    
+        compareResult = (leftElement < rightElement);
+    
+    /////////////////////////////////////////////////////////////////////////////    
+        /*add these to a single if else block*/
+        greater = compareResult ? rightElement : leftElement;
+        greater_cls = compareResult ? rightElement_cls : leftElement_cls;
+        lesser = compareResult ? leftElement : rightElement;
+        lesser_cls = compareResult ? leftElement_cls : rightElement_cls;
+    //////////////////////////////////////////////////////////////////////////////
+
+        distances[yIndex * n_refP + leftId] = lesser;
+        distances[yIndex * n_refP +rightId] = greater;
+    
+        clases[yIndex * n_refP + leftId] = lesser_cls;
+        clases[yIndex * n_refP +rightId] = greater_cls;
+    }
+   
+
+}
+
+
 
 int main()
 {
 
-    int n_refPoints = 8192;
-    int n_queryPoints = 2048;
+    // int n_refPoints = 8192;
+    // int n_queryPoints = 1024;
+    int n_refPoints = 16;
+    int n_queryPoints = 2;
     int n_dimentions = 4;
 
     float *refPoints_h, *refPoints_d;
@@ -40,7 +102,7 @@ int main()
     int *clases_h, *clases_d;
     float *queryPoints_h, *queryPoints_d;
 
-    float *distances_d;
+    float *distances_h, *distances_d;//distances_h not needed..only for test
 
     cudaError_t error;
     cudaDeviceProp prop;
@@ -75,8 +137,12 @@ int main()
     clases_h = (int *) malloc(sizeof(int) * n_refPoints * n_queryPoints);
     queryPoints_h = (float *)malloc(sizeof(float) * n_dimentions * n_queryPoints);
 
-    char *refPointsFileName = "testData8192_4.csv";
-    char *queryPointsFileName = "queryPoints_4.csv";
+    distances_h = (float *)malloc(sizeof(float)*n_refPoints*n_queryPoints);
+
+    // char *refPointsFileName = "testData8192_4.csv";
+    // char *queryPointsFileName = "queryPoints_4.csv";
+     char *refPointsFileName = "testData16_4.csv";
+    char *queryPointsFileName = "queryPoints1_4.csv";
 
     readRefPoints(refPointsFileName, refPoints_h, clases_h, n_refPoints, n_queryPoints, n_dimentions);
 
@@ -110,6 +176,11 @@ int main()
     cudaMemcpy(queryPoints_d, queryPoints_h, sizeof(float) * n_dimentions * n_queryPoints, cudaMemcpyHostToDevice);
     cudaMemcpy(clases_d, clases_h, sizeof(int) * n_refPoints * n_queryPoints, cudaMemcpyHostToDevice);
 
+    if (error != cudaSuccess)
+    {
+        printf("Error: %s\n", cudaGetErrorString(error));
+        exit(-1);
+    }
 
     int block_dim = warpSize / 2;
     int grid_dim = (n_refPoints / block_dim);
@@ -123,23 +194,100 @@ int main()
 
     cudaDeviceSynchronize();
     // cudaThreadSynchronize();
+
+    error = cudaGetLastError();
+
+    if (error != cudaSuccess)
+
+    {
+        printf("error in kernel\n");
+        printf("Error: %s\n", cudaGetErrorString(error));
+        exit(-1);
+    }
+
+
     
     printf("after kernel");
-    // cudaMemcpy(classAndDistArr_h, classAndDistArr_d, sizeof(ClassAndDist) * n_refPoints * n_queryPoints, cudaMemcpyDeviceToHost);
+    error = cudaMemcpy(distances_h, distances_d, sizeof(float) * n_refPoints * n_queryPoints, cudaMemcpyDeviceToHost);
 
-    // for(int i = 0; i<1 ; i++){
 
-    //     printf("hello");
+    if (error != cudaSuccess)
+    {
+        printf("Error: %s\n", cudaGetErrorString(error));
+        exit(-1);
+    }
 
-    //     printf("\n%f\n",_h[0].distance);
 
-    // }
+    printf("distances before sort\n");
+    for(int i = 0; i<n_refPoints ; i++){
+
+        // printf("hello");
+
+        printf("%f  ", i,distances_h[n_refPoints + i]);
+
+    }
+
+
+    /////////////////////////////////////////////////////////////////////////////////
+
+    // int block_count_x = (n_refPoints / 2) / warpSize;
+    // int block_count_y = n_queryPoints / warpSize;
+    
+
+    // block_size = dim3(warpSize, warpSize);
+    // block_size = dim3(warpSize, warpSize);
+    // grid_size = dim3(block_count_x, block_count_y);
+
+    block_size = dim3(8, 2);
+    grid_size = dim3(1, 1);
+
+
+    unsigned int numStages = 0, stage = 0, passOfStage = 0, temp = 0;
+
+    for (temp = n_refPoints; temp > 1; temp >>= 1)
+    {
+        ++numStages;
+    }
+
+    for (stage = 0; stage < numStages; ++stage)
+    {
+
+        for (passOfStage = 0; passOfStage < stage + 1; ++passOfStage)
+        {
+
+            sort_dist_bitonic<<<grid_size, block_size>>>(distances_d, clases_d, n_refPoints, n_queryPoints, stage, passOfStage);
+            cudaDeviceSynchronize();
+        }
+    }
+
+    error = cudaGetLastError();
+
+    if (error != cudaSuccess)
+
+    {
+        printf("error in sort kernel\n");
+        printf("Error: %s\n", cudaGetErrorString(error));
+        exit(-1);
+    }
+
+    error = cudaMemcpy(distances_h, distances_d, sizeof(float) * n_refPoints * n_queryPoints, cudaMemcpyDeviceToHost);
+    
+    printf("\n\ndistances after sort\n");
+    for(int i = 0; i<n_refPoints ; i++){
+
+        printf("%f  ", i,distances_h[n_refPoints + i]);
+
+    }
+    /////////////////////////////////////////////////////////////////////////////////
 
     cudaFree(refPoints_d);
     cudaFree(queryPoints_d);
+    cudaFree(clases_d);
+    cudaFree(distances_d);
     free(refPoints_h);
     free(queryPoints_h);
     free(clases_h);
+    free(distances_h);//not need if distances are not get back to host
 
     return 0;
 }
